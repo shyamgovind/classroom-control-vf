6a14fb902f03938556aca83f73dee2e0c3152338
ccox35 <31251526+ccox35@users.noreply.github.com>
2017-09-19 15:59:40 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -53,7 +53,7 @@ node default {
   }
    
   exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
-   path => '/usr/bin:/usr/local/bin,
+   path => '/usr/bin:/usr/local/bin',
    creates => '/etc/motd',
   }
 }
