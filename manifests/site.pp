e80531e7f6885adf4eb27429a0d93108e28ae207
testshyam <s.hyamgovind@gmail.com>
2017-09-19 15:30:58 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,10 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+
+file { '/etc/motd':
+   ensure => file,
+   content => "Today I learned what it means to manage state using Puppet. - From Shyam. \n",
+}
+
 }
