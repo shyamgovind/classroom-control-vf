2686fa55d2df9ab4dc5991e2cfdc4dc6fe016519
Rahul Nigam <rnigam@smu.edu>
2017-09-20 15:38:11 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -16,6 +16,27 @@ package { 'nginx':
  source => 'puppet:///modules/nginx/index.html',
  }
  
- Confgiru
- service { 'nginx'
+ # Configure nginx
+ file { '/etc/nginx/nginx.conf' :
+ ensure => file,
+ source => 'puppet:///modules/nginx/nginx.conf',
+ require => Package['nginx'],
+ }
+ 
+ file {'/etc/nginx/conf.d/default.conf' :
+ ensure => file,
+ source => 'puppet:///modules/nginx/default.conf',
+ require => Package['nginx'],
+ }
+ 
+ 
+ 
+ # Manage the service
+ service { 'nginx' :
+ ensure => running,
+ enable => true,
+ require => [File['/etc/nginx/conf.d/default.conf'], File['/etc/nginx/nginx.conf']],
+ }
+ 
+ }
  
