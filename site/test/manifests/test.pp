5693c4c7cedc5b40fe8213e6f0fbec32f581e76d
kevgig <kevgig@users.noreply.github.com>
2017-09-20 12:13:56 -0600

    Create test.pp

---
+++ site/test/manifests/test.pp
@@ -0,0 +1,11 @@
+class test {
+
+  file { '/etc/popooo':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => "Puppet go poo poo!\n",
+  }
+
+}
