16107dde5038dbd4e581bae21e2119357249817d
dkr003 <32079964+dkr003@users.noreply.github.com>
2017-09-20 10:50:28 -0700

    Update init.pp

--- site/Users/manifests/init.pp
+++ site/Users/manifests/init.pp
@@ -1,6 +1,6 @@
-class users {
-  user { 'fundamentals':
-  ensure => present,
+class user_setup {
+   user { 'fundamentals':
+   ensure => present,
 #   password => 'puppet8#labs', # Windows requires a plain text password
 #   groups => ['Users'], # Display in Windows Control Panel
   }
