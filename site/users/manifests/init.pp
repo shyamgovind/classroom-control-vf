acfa68b4c7008703eccc25b91e2f0633a7e9ba0e
kevgig <kevgig@users.noreply.github.com>
2017-09-20 16:17:51 -0600

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -7,5 +7,5 @@ class users {
   $my_name= "Kevin from local scope. i.e. clase users."
   
   notify { " Printing value from my_name from global manifest - ${::my_name}" :}
-  notify { " Printing value from my_name from local class - ${user::my_name}" :}
+  notify { " Printing value from my_name from local class - ${users::my_name}" :}
 }
