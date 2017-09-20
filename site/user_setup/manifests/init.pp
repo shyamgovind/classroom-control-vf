f716207ad69260a6c5d9ca6c9c1aa53c00d63556
testshyam <s.hyamgovind@gmail.com>
2017-09-20 15:15:24 -0700

    Update init.pp

--- site/user_setup/manifests/init.pp
+++ site/user_setup/manifests/init.pp
@@ -4,5 +4,11 @@ user { "fundamentals" :
   ensure => present,
 }
 
+$my_name = "Shyam from local scope. i.e. class user_setup."
+
+notify { " Printing value for my_name from global manifest - ${::my_name}" : }
+
+notify { " Printing value for my_name from local class - ${::user_setup::my_name} " : }
+
 
 }
