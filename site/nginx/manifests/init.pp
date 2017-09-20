e604ad1fdf673cc1a39d247f04e4ccd7301117b4
ccox35 <31251526+ccox35@users.noreply.github.com>
2017-09-20 13:34:34 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -6,43 +6,32 @@ class nginx {
   
   file { "/var/www":
     ensure => directory,
-    owner => root,
-    group => root,
   }
   
   file { "/var/www/index.html":
     ensure => file,
-    owner => root,
-    group => root,
     source => "puppet:///modules/nginx/index.html",
   }
   
   file { "/etc/nginx/nginx.conf":
     ensure => file,
-    owner => root,
-    group => root,
     source => "puppet:///modules/nginx/nginx.conf",
     require => Package['nginx'],
-    notify => service['nginx'],
   }
 
   file { "/etc/nginx/conf.d":
     ensure => directory,
-    owner => root,
-    group => root,
   }
 
   file { "/etc/nginx/conf.d/default.conf":
     ensure => file,
-    owner => root,
-    group => root,
     source => "puppet:///modules/nginx/default.conf",
     require => Package['nginx'],
-    notify => Service['nginx'],
   }
   
   service { "nginx":
     ensure => running,
     enabled => true,
+    require => [File['/etc/nginx/nginx.conf'],File['/etc/nginx/conf.d/default.conf']],
   }
 }
