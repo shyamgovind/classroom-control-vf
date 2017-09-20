eda1f77ea2dc8853d33debe5b3e7c64dd06b5829
kevgig <kevgig@users.noreply.github.com>
2017-09-20 12:32:37 -0600

    Create init.pp

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,15 @@
+class skeleton {
+  
+  file { '/etc/skel':
+    ensure => directory,
+    ownser => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  file { '/etc/skel/.bashrc':
+    ensure => file,
+    ownser => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/skeleton/bashrc',
+}
