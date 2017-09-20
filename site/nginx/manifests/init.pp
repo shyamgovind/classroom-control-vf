e51b64940ce5335de36fd0c62d3353eaa625d85d
Rahul Nigam <rnigam@smu.edu>
2017-09-20 15:24:56 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,1 +1,21 @@
+class nginx 
 
+# Package Installation
+package { 'nginx':
+ ensure => installed,
+ }
+ 
+ # Creating a WebPage and copying it to right location
+ file{ '/var/www/' :
+ ensure => directory,
+ }
+ 
+ 
+ file { '/var/www/index.html' :
+ ensure => file,
+ source => 'puppet:///modules/nginx/index.html',
+ }
+ 
+ Confgiru
+ service { 'nginx'
+ 
