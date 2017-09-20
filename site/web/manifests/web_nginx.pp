05af80eccb454d7d4f7f41012a55f611c30af9d0
kevgig <kevgig@users.noreply.github.com>
2017-09-20 14:19:23 -0600

    Update web_nginx.pp

--- site/web/manifests/web_nginx.pp
+++ site/web/manifests/web_nginx.pp
@@ -1,16 +1,54 @@
 class web::web_nginx {
 
 package { 'nginx' :
-  ensure => 'installed',
+  ensure => present,
   
 }
 
 file { '/var/www' :
-  ensure => 'directory',
+  ensure => directory,
+  owner => 'root',
+  group => 'root',
+  mode  => '0775',
+}
+
+file { '/var/www/index.html':
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode  => '0664',
+  source => 'puppet:///moudules/web/index.html',
 }
 
 file { '/etc/nginx/nginx.conf'
-  ensure => present,
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode  => '0664',
   source => 'puppet:///moudules/web/nginx.conf',
-
+  reequire => Package['web_nginx'],
+  notify   => Service['web_nginx'],
 }
+
+file { '/etc/nginx/conf.d':
+  ensure => directory,
+  owner => 'root',
+  group => 'root',
+  mode  => '0775',
+}
+
+file { '/etc/nginx/conf.d/default.conf'
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode  => '0664',
+  source => 'puppet:///moudules/web/default.conf',
+  reequire => Package['web_nginx'],
+  notify   => Service['web_nginx'],
+}
+
+service {'nginx':
+  ensure => running,
+  enable => true,
+}
+
