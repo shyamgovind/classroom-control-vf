913d837f7f3d4462bb7e0451276a7bc5360eb48f
kevgig <kevgig@users.noreply.github.com>
2017-09-20 16:15:38 -0600

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -3,4 +3,9 @@ class users {
   user { 'fundamentals':
     ensure => present,
   }
+  
+  $my_name= "Kevin from local scope. i.e. clase users."
+  
+  notify { " Printing value from my_name from global manifest - ${::my_name}" :}
+  notify { " Printing value from my_name from local class - ${user::my_name}" :}
 }
