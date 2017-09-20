4d3a32b82c5780a7b3b1a32f1cc28f69e498515e
ccox35 <31251526+ccox35@users.noreply.github.com>
2017-09-20 12:02:59 -0700

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -4,7 +4,7 @@ class skeleton {
   ensure => directory,
   }
   
-  file { 'etc/skel/.bashrc':
+  file { '/etc/skel/.bashrc':
   ensure => file,
   source => 'puppet:///modules/skeleton/bashrc',
   }
