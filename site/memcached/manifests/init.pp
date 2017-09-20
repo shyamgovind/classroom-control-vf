9bb7edd90b1683b85c218e1218f84e6322723cff
dkr003 <32079964+dkr003@users.noreply.github.com>
2017-09-20 14:50:19 -0700

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,15 @@
+class memcached {
+  package { 'memcached':
+    ensure => present,
+    }
+  file { '/etc/sysconfig/memcached':
+    ensure => file,
+    source => 'puppet:///modules/memcached/memcached',
+    require => Package['memcached'],
+    }
+  service { 'memcached':
+    ensure => running,
+    enable => true,
+    subscribe => File['/etc/sysconfig/memcached'],
+    }
+}
