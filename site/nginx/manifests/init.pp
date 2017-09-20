a4981a49a2591c3ecf8e167e7d764077dbda9a43
Omri Gazitt <ogazitt@gmail.com>
2017-09-20 13:16:01 -0700

    create nginx class

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,62 @@
+class nginx {
+  package { 'nginx':
+    ensure => present,
+  }
+  
+  service { 'nginx':
+  }
+  
+  file { '/var/www':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
+  }
+
+  file { '/var/www/index.html':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',    
+    source => 'puppet:///modules/nginx/index.html',
+  }
+
+  file { '/etc/nginx': 
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
+  }
+  
+  file { '/etc/nginx/nginx.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet:///modules/nginx/nginx.conf',
+    #require => Package['nginx'],
+    #notify => Service['nginx'],
+  }
+
+  file { '/etc/nginx/conf.d':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
+  }
+
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet:///modules/nginx/default.conf',
+    #require => Package['nginx'],
+    #notify => Service['nginx'],
+  }
+  
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+}
