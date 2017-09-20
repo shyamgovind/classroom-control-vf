c790b8325c71560a6c1bc5738f4522f32d1477e5
testshyam <s.hyamgovind@gmail.com>
2017-09-20 13:37:59 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -17,8 +17,24 @@ file { "/var/www/" :
 
 # Configure nginx
 
+file { "/etc/nginx/nginx.conf" :
+  ensure => file,
+  source => "puppet:///modules/nginx/nginx.conf",
+  require => Package['nginx'],
+  }
 
-
+file { "/etc/nginx/conf.d/default.conf" :
+  ensure => file,
+  source => "puppet:///modules/nginx/default.conf",
+  require => Package['nginx'],
+  }
+  
+# Manage the service 
+service { "nginx" :
+  ensure => running,
+  enable => true,
+  subscribe => [ File['/etc/nginx/nginx.conf'],File['/etc/nginx/conf.d/default.conf']],
+  }
 
 
 }
