19e4b0d8eb68986d088c955d62c9d388a3552119
chasbutt <csb75@me.com>
2017-09-20 13:32:03 -0700

    init.pp for nginx

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,42 @@
+class nginx {
+  package { "nginx" :
+    ensure => installed,
+  }
+  
+  file { "/var/www/":
+    ensure => directory,
+  }
+  
+  file { "/var/www/index.html" :
+    ensure => file,
+    source => "puppet:///modules/nginx/index.html",
+  }
+  file { "/etc/nginx/nginx.conf":
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+  file { '/etc/nginx/conf.d':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
+  }
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+  service {'nginx':
+    ensure => running,
+    enable => true,
+  }
+}
