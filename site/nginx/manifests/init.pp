9368dfa0e8b2774a66cca459c2511bd285179ddb
dkr003 <32079964+dkr003@users.noreply.github.com>
2017-09-20 13:42:58 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,32 +1,32 @@
 class nginx {
-  package { "nginx" }
+  package { 'nginx' :
     ensure => installed
   }
-  file  {"/var/www/" :
+  file  {'/var/www/' :
    ensure => directory,
    }
   
-  file { "/var/www/index.html" :
+  file { '/var/www/index.html' :
     ensure => file,
-    source => "puppet:///modules/nginx/index.html",
+    source => 'puppet:///modules/nginx/index.html',
     }
 
 #Configure NGINX
 
-  file { "/etc/nginx/nginx.conf" :
+  file { '/etc/nginx/nginx.conf' :
     ensure => file,  
-    source "puppet:///modules/nginx/nginx.conf",
+    source 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
    }
    
-  file { "/etc/nginx/conf.d/default.conf" :
+  file { '/etc/nginx/conf.d/default.conf' :
     ensure => file,
-    source => "puppet:///modules/nginx/default.conf",
+    source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
     }
 
 #Manage the service
-service { "nginx" :
+service { 'nginx' :
   ensure => running,
   enable => true,
   subscribe => [File ['/etc/nginx/nginx.conf'],File ['/etc/nginx/conf.d/default.conf']]
