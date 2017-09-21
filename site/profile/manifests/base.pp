a547d8ac151e791e248b738a30fabb8eb98164ce
chasbutt <csb75@me.com>
2017-09-21 14:18:40 -0700

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,7 +1,5 @@
 class profile::base {
-  notify { "Hello, my name is NOT ${::hostname}" or is it: }
+  notify { "Hello, my name is NOT ${::hostname} or is it": }
   # $message = hiera('message')
   # notify { $message: }
 }
-
-#another line
