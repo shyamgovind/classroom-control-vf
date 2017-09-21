5e3b4df140a16167b0ab2137a0004fb44f98d7f9
testshyam <s.hyamgovind@gmail.com>
2017-09-21 11:20:12 -0700

    Update basic_env_stuff.pp

--- manifests/basic_env_stuff.pp
+++ manifests/basic_env_stuff.pp
@@ -3,3 +3,12 @@ user { "audit_user" :
    }
 
 $my_name = "Shyam from global scope"
+
+#include nginx   # takes the default "/var/www" as the doc_root parameter.
+
+
+class { "nginx" :
+  doc_root => "/var/website/",
+  }
+
+
